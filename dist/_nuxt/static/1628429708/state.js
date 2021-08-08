68d0d2da9533ce6568e2edcb79fb7b3114b9a918
RosanaRufer <rosanarufer@gmail.com>
2021-08-08 14:35:18 +0100

    f

---
+++ dist/_nuxt/static/1628429708/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628429708",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8ec1418a"}}}}(null,"\u002F"));
